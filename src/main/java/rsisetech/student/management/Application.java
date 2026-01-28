2322c521e84eb1245fb4a909fcc8cc91ed149ca6
Keito Kobayashi <xiaolinxiren@gmail.com>
2026-01-28 20:26:52 +0900

    raise-tech18完了

--- src/main/java/rsisetech/student/management/Application.java
+++ src/main/java/rsisetech/student/management/Application.java
@@ -4,21 +4,35 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Application {
 	private String name = "Enami Kouji";
+	private  String age = "37";
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@GetMapping("/name")
-	public String getName() {
-		return name;
+	@GetMapping("/studentInfo")
+	public String getStudentInfo(){
+		return name + " " + age + "歳";
+	}
+
+	@PostMapping("/studentInfo")
+	public void setStudentInfo(String name,String age){
+	this.name = name;
+	this.age = age;
+	}
+
+	@PostMapping("/name")
+	public void updateStudentName(String name){
+		this.name = name;
+	}
+	@PostMapping("/age")
+	public void updateStudentAge(String age){
+		this.age = age;
 	}
 	}
-
-
-
