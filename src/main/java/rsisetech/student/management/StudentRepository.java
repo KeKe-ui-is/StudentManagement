8fc9536ed006e93dfc2953db012fb2d6c41f924f
Keito Kobayashi <xiaolinxiren@gmail.com>
2026-02-27 15:42:09 +0900

    raise-tech23　students students_coursesテーブルからjson形式で情報を取得してクラスに格納できるように機能追加

--- src/main/java/rsisetech/student/management/StudentRepository.java
+++ src/main/java/rsisetech/student/management/StudentRepository.java
@@ -2,17 +2,19 @@ package rsisetech.student.management;
 
 import org.apache.ibatis.annotations.*;
 
+import java.util.List;
+
 @Mapper
 public interface StudentRepository {
-    @Select("SELECT * FROM student WHERE name = #{name}")
-    Student searchByName(String name);
+    @Select("SELECT * FROM students")
+    List<Student> search();
 
-    @Insert("INSERT student values(#{name},#{age})")
-    void registerStudent(String name,int age);
-
-    @Update("UPDATE student SET age = #{age} WHERE name = #{name}")
-    void updateStudent(String name ,int age);
-
-    @Delete("DELETE FROM student WHERE name = #{name}")
-    void deleteStudent(String name);
+//    @Insert("INSERT student values(#{name},#{age})")
+//    void registerStudent(String name,int age);
+//
+//    @Update("UPDATE student SET age = #{age} WHERE name = #{name}")
+//    void updateStudent(String name ,int age);
+//
+//    @Delete("DELETE FROM student WHERE name = #{name}")
+//    void deleteStudent(String name);
 }
