ac0c0c2cb2879043f135e7de00cd88c255f7e2dc
Keito Kobayashi <xiaolinxiren@gmail.com>
2026-03-03 20:31:39 +0900

    Converterのクラスを実装することでテーブルから取り出したデータをまとめたり、加工することができるようになった。 converter　値を詰め替えしている　サービスからとってきた情報をもとに画面に返す必要な情報を組み立てるこの組み立てる処理をコンバーターという
    StudentDetail生成用Converterを追加
    
    ・StudentとStudentsCoursesをIDで紐付け
    ・EntityからDomainモデルへの変換処理を実装
    ・レスポンス用データ構築ロジックを追加

--- src/main/java/rsisetech/student/management/controller/StudentController.java
+++ src/main/java/rsisetech/student/management/controller/StudentController.java
@@ -3,44 +3,40 @@ package rsisetech.student.management.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import rsisetech.student.management.controller.converter.StudentConverter;
 import rsisetech.student.management.data.Student;
 import rsisetech.student.management.data.StudentsCourses;
+import rsisetech.student.management.domain.StudentDetail;
 import rsisetech.student.management.service.StudentService;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class StudentController {
 
     private StudentService service;
+    private  StudentConverter converter;
     @Autowired
-    public StudentController(StudentService service) {
+    public StudentController(StudentService service, StudentConverter converter) {
         this.service = service;
+        this.converter = converter;
     }
 
-    @GetMapping("/student")
-    public List<Student> getStudentList(){
-        return service.searchStudentList();
+    @GetMapping("/studentList")
+    public List<StudentDetail> getStudentList(){
+        List<Student> students = service.searchStudentList();
+        List<StudentsCourses> studentsCourses = service.searchStudentsCoursesList();
+
+        return converter.convertStudentDetails(students, studentsCourses);
     }
 
-    @GetMapping("/studentAge30")
-    public List<Student> getStudentAge30(){
-        return service.searchStudentListMinAge30();
-    }
 
     @GetMapping("/studentsCoursesList")
     public List<StudentsCourses> getStudentsCourses(){
         return service.searchStudentsCoursesList();
     }
 
-    @GetMapping("/studentsCoursesNameJava")
-    public  List<StudentsCourses> getStudentsCoursesNameJava(){
-        return service.searchStudentsCoursesNameJava();
-    }
 
-    //課題３０代以上の人のみ抽出する
-    //抽出したリストをコントローラーに返す
-
-    //Javaフルコースの情報のみを抽出する
-    //抽出したリストを返す
 }
--- src/main/java/rsisetech/student/management/data/Student.java
+++ src/main/java/rsisetech/student/management/data/Student.java
@@ -7,11 +7,15 @@ import lombok.Setter;
     @Setter
 public class Student {
     private String name;
+    private String id;
     private String kanaName;
     private String nickname;
     private String email;
     private String area;
     private int age;
     private String sex;
+    //24課題下の2種類のフィールドに対応するレコードをStudentsテーブルに追加　追加はALTER TABLEを使う
+    private String remark;
+    private boolean isDeleted;
 
 }
