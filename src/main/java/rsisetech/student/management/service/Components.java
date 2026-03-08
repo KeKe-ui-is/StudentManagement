93fa358a79c1e8bfe147cdd4545577780702598c
Keito Kobayashi <xiaolinxiren@gmail.com>
2026-03-08 19:32:40 +0900

    registerStudent.html に受講生情報を入力するフォームを作成 Componentsクラスにテーブルの一番大きいidの次の順番のidを生成するメソッドを実装 テーブルのidを降順に並べ替えその先頭を最後のidとしてそれを引数にして作成することで自動的にidを生成できるようになった。001-999まで対応 StudentRepositoryとStudentsCoursesRepositoryにテーブルにデータを追加する処理を実装
    Serviceのほうには
    addStudent（）受講生情報を登録
    addStudentCourses()コース名などを登録
    addStudentsAndCourses（）上記２つのメソッドを同時に実行できるようにまとめたもの
    を実装
    /newStudentにGetMappingすることで入力フォームへ遷移
    入力が成功すると/studentListにGetMappingされる
    
    次回実装予定のもの
     入力されるデータのチェック
     StudentServiceに受講開始日と終了予定のデータを生成して渡すようにする処理の実装

---
+++ src/main/java/rsisetech/student/management/service/Components.java
@@ -0,0 +1,11 @@
+package rsisetech.student.management.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Components {
+    String createId(String currentMaxId,Character s){
+        int number = Integer.parseInt(currentMaxId.substring(2));
+        return String.format("%c-%03d",s,number + 1);
+    }
+}
