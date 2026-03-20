df34fa33eaccc9248948603aa604d080ced857a3
Keito Kobayashi <xiaolinxiren@gmail.com>
2026-03-20 13:27:41 +0900

    Studentのageの型をintからIntegerに変更 また@Sizeなどのアノテーションの整理 すべてのメッセージを外部化し入力チェック時メッセージを返せるように変更

--- src/main/java/raisetech/student/management/data/Student.java
+++ src/main/java/raisetech/student/management/data/Student.java
@@ -4,36 +4,45 @@ import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 
+/*
+  受講生情報を扱うオブジェクト
+*/
 @Getter
 @Setter
-    /*
-      受講生情報を扱うオブジェクト
-     */
 public class Student {
+
     private String id;
-    @Size(min = 1, max = 50)
+    // 名前
     @NotBlank(message = "{student.name.required}")
+    @Size(min = 1, max = 50, message = "{student.name.size}")
     private String name;
-    @Size(min = 1, max = 50)
+    // カナ名
     @NotBlank(message = "{student.kanaName.required}")
+    @Size(min = 1, max = 50, message = "{student.kanaName.size}")
     private String kanaName;
-    @Size(min = 1, max = 50)
-    @NotBlank
+    // ニックネーム
+    @NotBlank(message = "{student.nickname.required}")
+    @Size(min = 1, max = 50, message = "{student.nickname.size}")
     private String nickname;
-    @Size(min = 1, max = 50)
-    @Email(message = "{student.email.invalid}")
-    @Size(max = 50)
+    // メール
     @NotBlank(message = "{student.email.required}")
+    @Email(message = "{student.email.invalid}")
+    @Size(max = 50, message = "{student.email.size}")
     private String email;
-    @Size(min = 1, max = 50)
+    // 地域
+    @Size(min = 1, max = 50, message = "{student.area.size}")
     private String area;
+    // 年齢
+    @NotNull(message = "{student.age.required}")
     @Min(value = 0, message = "{student.age.min}")
     @Max(value = 100, message = "{student.age.max}")
-    private int age;
-    @Size(min = 1, max = 10)
+    private Integer age;
+    // 性別
+    @Size(min = 1, max = 10, message = "{student.sex.size}")
     private String sex;
-    @Size(max = 255)
+    // 備考
+    @Size(max = 255, message = "{student.remark.size}")
     private String remark;
-    @NotNull
+    // 削除フラグ
     private boolean deleted;
 }
