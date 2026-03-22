18385d032ca4b82002a540e4c59ecb4dd54ede4d
Keito Kobayashi <xiaolinxiren@gmail.com>
2026-03-22 09:44:05 +0900

    Open APIの実装によるドキュメント生成の実装
    StudentController
    Student
    StudentCourse
    StudentDetail
    にOpenAPIのアノテーションを追加
    
    使用方法
    アプリを起動後
    http://localhost:8080/swagger-ui/index.html
    にアクセス

--- src/main/java/raisetech/student/management/controller/StudentController.java
+++ src/main/java/raisetech/student/management/controller/StudentController.java
@@ -1,8 +1,15 @@
 package raisetech.student.management.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import org.apache.ibatis.annotations.Options;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -37,6 +44,16 @@ public class StudentController {
      *
      * @return 受講生詳細一覧（全件数）
      */
+    @Operation(
+            summary = "一覧検索",
+            description = "受講生の一覧を検索します",
+            operationId = "getStudentList",
+            tags = {"SearchStudent"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "検索成功"),
+            @ApiResponse(responseCode = "500",description = "サーバー内部エラー")
+    })
     @GetMapping("/studentList")
     public List<StudentDetail> getStudentList() {
         return service.searchStudentList();
@@ -49,8 +66,23 @@ public class StudentController {
      * @param id 受講生ID
      * @return 受講生情報とそれに紐づく受講コース情報
      */
+    @Operation(
+            summary = "個別検索",
+            description = "入力されたidに紐づく受講生を検索します",
+            operationId = "getStudent",
+            tags = {"SearchStudent"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "検索成功"),
+            @ApiResponse(responseCode = "500",description = "サーバー内部エラー")
+    })
     @GetMapping("/student/{id}")
-    public StudentDetail getStudent(@PathVariable @Size(min = 1, max = 3) @NotNull String id) {
+    public StudentDetail getStudent(
+            @Parameter(description = "受講生ID 1～3文字で入力",required = true)
+            @PathVariable
+            @Size(min = 1, max = 3)
+            @NotNull
+            String id) {
         return service.searchStudent(id);
     }
 
@@ -62,6 +94,26 @@ public class StudentController {
      * @param studentDetail 受講生詳細
      * @return 実行結果
      */
+    @Operation(
+            summary = "受講生情報登録",
+            description = "受講生と受講コースの情報を登録します。",
+            tags = {"UpdateStudentDB"}
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "登録成功",
+                    content = @Content(schema = @Schema(implementation = StudentDetail.class))
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "入力値が不正です"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "サーバー内部エラー"
+            )
+    })
     @PostMapping("/registerStudent")
     public ResponseEntity<StudentDetail> registerStudent(@RequestBody @Valid StudentDetail studentDetail) {
         service.registerStudent(studentDetail);
@@ -73,8 +125,28 @@ public class StudentController {
      *
      * @param studentDetail 受講生詳細
      */
+    @Operation(
+            summary = "受講生情報更新",
+            description = "受講生と受講コースの情報を更新します。",
+            tags = {"UpdateStudentDB"}
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "更新成功",
+                    content = @Content(schema = @Schema(implementation = StudentDetail.class))
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "入力値が不正です"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "サーバー内部エラー"
+            )
+    })
     @PutMapping("/updateStudent")
-    public ResponseEntity<String> updateStudent(@RequestBody StudentDetail studentDetail) {
+    public ResponseEntity<String> updateStudent(@RequestBody @Valid StudentDetail studentDetail) {
         //更新処理
         service.updateStudent(studentDetail);
         return ResponseEntity.ok("更新処理が成功しました。");
--- src/main/java/raisetech/student/management/domain/StudentDetail.java
+++ src/main/java/raisetech/student/management/domain/StudentDetail.java
@@ -1,5 +1,7 @@
 package raisetech.student.management.domain;
 
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,13 +12,16 @@ import raisetech.student.management.data.StudentCourse;
 
 import java.util.List;
 
+@Schema(description = "受講生詳細")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class StudentDetail {
+    @Schema(description = "受講生情報")
     @Valid
     private Student student;
+    @ArraySchema(schema = @Schema(description = "１つから複数の受講生コース情報"))
     @Valid
     private List<StudentCourse> studentCourseList;
 }
